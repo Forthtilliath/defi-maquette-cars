218ae8635830cb21d128aa94e27e0e95d8ee8761
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-08-08 00:23:37 +0200

    Script paiement

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+const currencyInputs = document.querySelectorAll("input[data-type='currency']");
+
+window.onload = () => {
+    rangeInputs.forEach((input) => {
+        handleInputChange(input);
+        setCurrency(input);
+    });
+};
+
+rangeInputs.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        handleInputChange(e.target);
+        setCurrency(e.target);
+    });
+});
+
+currencyInputs.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        formatCurrency(e.target);
+        setRange(e.target);
+    });
+});
+
 /****************************************
  * Input range                          *
  ****************************************/
 
-const rangeInputs = document.querySelectorAll('input[type="range"]');
-// const numberInput = document.querySelector('input[type="number"]');
-
+/**
+ * Adapte la longueur du background en fonction de la valeur du champ
+ * @param {HTMLInputElement} target Input de type range
+ */
 function handleInputChange(target) {
-    // let target = e.target;
-    if (target.type !== 'range') {
-        target = document.getElementById('range');
-    }
     const min = target.min;
     const max = target.max;
     const val = target.value;
@@ -17,61 +38,90 @@ function handleInputChange(target) {
     target.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%';
 }
 
-rangeInputs.forEach((input) => {
-    input.addEventListener('input', (e) => handleInputChange(e.target));
-});
-
-window.onload = () => {
-    rangeInputs.forEach((input) => {
-        handleInputChange(input);
-    });
-};
-
-function formatNumber(n) {
-    // format number 1000000 to 1.234.567
-    return n.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+/**
+ * Retourne le champ Range correspondant au champ currency
+ * @param {HTMLInputElement} input
+ * @returns {HTMLInputElement}
+ */
+function getClosestRange(input) {
+    return input.closest('#paiement > div').querySelector('input[type="range"]');
 }
 
-// numberInput.addEventListener('input', handleInputChange);
+/**
+ * Met à jour la valeur du champ range à partir du champ input
+ * @param {HTMLInputElement} inputCurrency
+ */
+function setRange(inputCurrency) {
+    const inputRange = getClosestRange(inputCurrency);
+    inputRange.value = formatInputToNumber(inputCurrency.value);
+    handleInputChange(inputRange);
+}
+
+function getClosestInput(input) {
+    return (
+        input.closest('#paiement > div').querySelector('input[type="text"]') ||
+        input.closest('#paiement > div').querySelector('select')
+    );
+}
+
+/**
+ * Met à jour la valeur du champ input à partir du champ range
+ * @param {HTMLInputElement} inputRange
+ */
+function setCurrency(inputRange) {
+    const input = getClosestInput(inputRange);
+    input.value = inputRange.value;
+
+    if (input.getAttribute('data-type') === 'currency') {
+        formatCurrency(input);
+    }
+}
 
 /****************************************
  * Input currency                       *
  ****************************************/
 
-const currencyInputs = document.querySelectorAll("input[data-type='currency']");
-currencyInputs.forEach((input) => {
-    input.addEventListener('keyup', (e) => formatCurrency(e.target));
-    input.addEventListener('blur', (e) => formatCurrency(e.target));
-});
-
+/**
+ * Ajoute des points pour les milliers
+ * @param {String} n
+ * @returns {String}
+ */
 function formatNumber(n) {
     // format number 1000000 to 1.234.567
     return n.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
-function formatInput(n) {
+
+/**
+ * Convertit la valeur du champ en nombre (retire les points et les 0 inutiles)
+ * @param {String} n
+ * @returns {Number}
+ */
+function formatInputToNumber(n) {
     // format number 1.000.000 to 1234567
     let nWithoutPoint = n.replace('.', '');
     let nNumber = parseInt(nWithoutPoint, 10);
-    let nString = nNumber.toString();
-    return nString;
+    return nNumber;
 }
 
+/**
+ * Met à jour l'affichage du prix
+ * @param {HTMLInputElement} input Input de type text
+ * @returns
+ */
 function formatCurrency(input) {
-    let input_val = formatInput(input.value);
-
-    if (input_val === '') {
-        return;
-    }
+    if (input.value === '') return;
 
     let original_len = input.value.length;
 
     // position du curseur
     let caret_pos = input.selectionStart;
 
-    input.value = formatNumber(input_val);
+    // met à jour la valeur dans le champ
+    input.value = formatNumber(formatInputToNumber(input.value).toString());
 
     // repositionne le curseur
     let updated_len = input.value.length;
     caret_pos = updated_len - original_len + caret_pos;
+
     input.setSelectionRange(caret_pos, caret_pos);
 }
